2a9cb75d12dd16cad79bb63a181be2de13b0840a
Madalina Pana <44752259+panamadalina@users.noreply.github.com>
2024-04-16 07:41:59 +0300

    Add files via upload

---
+++ Seminar8/facade/ProgMain.java
@@ -0,0 +1,19 @@
+package facade;
+
+public class ProgMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//
+//		Airline company=new Airline("CTS flughts");
+//		Flight toGo=company.flightBooking("Buchares", "Cluj");
+//		Flight back=company.flightBooking("cluj", "Bucharest");
+//		
+//		Hotel hotel=new Hotel("CTS Hotel");
+//		hotel.roomBooking("cluj");
+		
+		Facade.reservation("Bucharest","Cluj");
+		
+	}
+
+}
